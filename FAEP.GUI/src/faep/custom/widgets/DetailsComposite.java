dc0092d44c5750924897ef680f37b752a627f049
jindster <jindzo7@gmail.com>
2013-09-22 21:14:24 +0300

    stable version before major refactoring in expanditem area.

--- FAEP.GUI/src/faep/custom/widgets/DetailsComposite.java
+++ FAEP.GUI/src/faep/custom/widgets/DetailsComposite.java
@@ -10,62 +10,61 @@ import org.eclipse.swt.widgets.Text;
 
 import common.wrappers.Project;
 
-public class DetailsScrolledComposite extends ScrolledComposite {
+public class DetailsComposite extends Composite {
 
     private Text text1;
     private Text text9;
     private Text text10;
 
-    public DetailsScrolledComposite(Composite parent, int style) {
+    public DetailsComposite(Composite parent, int style) {
 	super(parent, style);
-	// TODO Auto-generated constructor stub
     }
 
-    public void initMainComposite(Project project) {
-	// scrolledComposite = new ScrolledComposite(this.getParent(), SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-	// TODO set scrolled composite as controller for ExpandItem
-	// this.setControl(scrolledComposite);
-	this.setExpandHorizontal(true);
-	this.setExpandVertical(true);
+    public DetailsComposite(ScrolledComposite parent, int style, Project project) {
+	super(parent, style);
+	initMainComposite(parent, project);
+    }
 
-	Composite composite = new Composite(this, SWT.NONE);
+    public void initMainComposite(ScrolledComposite parent, Project project) {
+
+	// Composite composite = new Composite(parent, SWT.NONE);
 	// this.setHeight(300);
-	composite.setLayout(new GridLayout(4, false));
+	this.setLayout(new GridLayout(4, false));
 
-	text1 = addTextColumns("Status:", project.getState(), composite, false);
+	text1 = addTextColumns("Status:", project.getState(), this, false);
 	text1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 
-	Text text5 = addTextColumns("Project Creator:", project.getBuyerUserName(), composite, false);
+	Text text5 = addTextColumns("Project Creator:", project.getBuyerUserName(), this, false);
 	text5.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 
-	Text text2 = addTextColumns("Providers:", "Nyest", composite, false);
+	Text text2 = addTextColumns("Providers:", "Nyest", this, false);
 	text2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 
-	Text text6 = addTextColumns("Bid Count:", project.getBidCount() + "", composite, false);
+	Text text6 = addTextColumns("Bid Count:", project.getBidCount() + "", this, false);
 	text6.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 
-	Text text3 = addTextColumns("Budget:", project.getMinBudget() + " - " + project.getMaxBudget(), composite, false);
+	Text text3 = addTextColumns("Budget:", project.getMinBudget() + " - " + project.getMaxBudget(), this, false);
 	text3.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 
-	Text text7 = addTextColumns("Avarage Bid:", project.getAverageBidVal() + "", composite, false);
+	Text text7 = addTextColumns("Avarage Bid:", project.getAverageBidVal() + "", this, false);
 	text7.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 
-	Text text4 = addTextColumns("Created:", project.getStartDate().toString(), composite, true);
+	Text text4 = addTextColumns("Created:", project.getStartDate().toString(), this, true);
 	text4.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 2));
 
-	Text text8 = addTextColumns("Ends:", project.getEndDate().toString(), composite, true);
+	Text text8 = addTextColumns("Ends:", project.getEndDate().toString(), this, true);
 	text8.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 2));
 
-	text9 = new Text(composite, SWT.WRAP | SWT.MULTI | SWT.BORDER);
+	text9 = new Text(this, SWT.WRAP | SWT.MULTI | SWT.BORDER);
 	text9.setEditable(false);
 	text9.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 4, 1));
 	text9.setText(project.getShortDescription());
 
-	Label lblJobType = new Label(composite, SWT.NONE);
+	Label lblJobType = new Label(this, SWT.NONE);
 	lblJobType.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, false, false, 1, 3));
 	lblJobType.setText("Job Type:");
 
-	text10 = new Text(composite, SWT.READ_ONLY | SWT.WRAP | SWT.MULTI);
+	text10 = new Text(this, SWT.READ_ONLY | SWT.WRAP | SWT.MULTI);
 	text10.setEditable(false);
 	text10.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 1, 3));
 	text10.setText(project.getJobCategoryAsString("\n"));
@@ -86,4 +85,17 @@ public class DetailsScrolledComposite extends ScrolledComposite {
 	textField.setText(fieldText);
 	return textField;
     }
+
+    public Text getText1() {
+	return this.text1;
+    }
+
+    public Text getText9() {
+	return this.text9;
+    }
+
+    public Text getText10() {
+	return this.text10;
+    }
+
 }

f827aa4f3aa3c693d59e7cf4f12c8ef71735ec85
jindster <jindzo7@gmail.com>
2014-01-12 18:23:33 +0200

    Changed Color of messages.

--- Merc.GUI/src/merc/gui/constants/MercColor.java
+++ Merc.GUI/src/merc/gui/constants/MercColor.java
@@ -10,9 +10,9 @@ public class MercColor {
     public static Color WHITE = new Color(Display.getCurrent(), 255, 255, 255);
     public static Color BLACK = new Color(Display.getCurrent(), 0, 0, 0);
     public static Color TEXT_ERROR = new Color(Display.getCurrent(), 255, 200, 200);
-    public static Color BLUE = new Color(Display.getCurrent(), 144, 207, 255);
-    public static Color LIGHTBLUE = new Color(Display.getCurrent(), 176, 221, 255);
-    public static Color LIGHTORANGE = new Color(Display.getCurrent(), 255, 221, 176);
-    public static Color ORANGE = new Color(Display.getCurrent(), 255, 197, 121);
+    public static Color BLUE = new Color(Display.getCurrent(), 191, 200, 205);
+    public static Color LIGHTBLUE = new Color(Display.getCurrent(), 224, 233, 238);
+    public static Color LIGHTORANGE = new Color(Display.getCurrent(), 229, 220, 208);
+    public static Color ORANGE = new Color(Display.getCurrent(), 198, 188, 176);
 
 }
